940281d174a1124468aa4c7de29a6f0a2090336b
kiltec <53696055+pwnedu@users.noreply.github.com>
2022-10-20 16:21:05 +1100

    Update Packages.cs

--- Editor/Packages.cs
+++ Editor/Packages.cs
@@ -10,6 +10,9 @@ public class Packages
     public const string myNotesPackage = "com.kiltec.mynotes";
     public const string myNotesName = "My Notes";
 
+    public const string scriptEditorPackage = "com.kiltec.scripteditor";
+    public const string scriptEditorName = "Script Editor";
+
     public const string projectCustomisationPackage = "com.kiltec.projectcustomiser";
     public const string projectCustomisationName = "Project Customisation";
 
